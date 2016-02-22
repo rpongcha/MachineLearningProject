19cd37917706d3ddc00b8f64351832b2c2059771
rpongcha <rpongcha@hawk.iit.edu>
2016-02-22 15:40:08 -0600

    work both bottom and head

--- Utils.py
+++ Utils.py
@@ -12,11 +12,14 @@ def read_price_from_csv(key, length, bottom=True):
         with open(filename, 'rb') as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             rows = list(reader)
-            start = len(rows) - length
-            prices = rows[start:]
+            if bottom:
+                start = len(rows) - length
+                prices = rows[start:]
+            else:
+                prices = rows[0:length]
 
     return prices
 
 
 if __name__ == '__main__':
-    read_price_from_csv('AD', 12500)
+    read_price_from_csv('BD', 12500, False)
